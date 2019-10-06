19f3cbb898cb706bd6acd4727d51f022355a0261
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2019-10-06 16:43:29 +0100

    props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import Table from './Table'
 
 class App extends Component {
     render() {
+
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Motolani',
+                job: 'Banker',
+            },
+            {
+                name: 'Iskilu',
+                job: 'Artist',
+            },
+            {
+                name: 'Buhari',
+                job: 'Failure',
+            },
+        ]
+
         return (
             <div className="container">
                 
-                <Table />
+                <Table characterData={characters} />
                
             </div>
         )
--- src/Table.js
+++ src/Table.js
@@ -1,48 +1,40 @@
 import React, { Component } from "react";
 
 const TableHeader = () => {
-    return (
-        <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    )
-}
-
-const TableBody = () => {
   return (
-    <tbody>
+    <thead>
       <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
+        <th>Name</th>
+        <th>Job</th>
       </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
+    </thead>
+  );
+};
+
+const TableBody = props => {
+
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
       </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  )
-}
+    )
+  })
+
+  return <tbody>{rows}</tbody>
+};
 
 class Table extends Component {
   render() {
-    return (
-        
-        <table>
-            <TableHeader />
-            <TableBody />
-        </table>
 
+    const { characterData } = this.props
+
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} />
+      </table>
     );
   }
 }
