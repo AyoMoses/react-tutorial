a89fd4c17089c3ad25893db18cff2f406a88b734
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2019-10-16 10:13:32 +0100

    State

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
+    removeCharacter = index => {
+
+        const { characters } = this.state
+
+        this.setState({
+
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+    state = {
+        characters: [],
+        
+    }
+   
     render() {
 
-        const characters = [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Motolani',
-                job: 'Banker',
-            },
-            {
-                name: 'Iskilu',
-                job: 'Artist',
-            },
-            {
-                name: 'Buhari',
-                job: 'Failure',
-            },
-        ]
+        const { characters } = this.state
 
         return (
             <div className="container">
                 
-                <Table characterData={characters} />
-               
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form />
             </div>
         )
     }
